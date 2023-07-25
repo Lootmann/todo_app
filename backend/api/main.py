26b568ca59735c31a5952bbd57434ee670b1b814
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-07-25 22:03:17 +0900

    CORS from React:Vite

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers.todos import router as todo_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # default vite port
+    "http://localhost:5174",  # next vite port
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(todo_router)
